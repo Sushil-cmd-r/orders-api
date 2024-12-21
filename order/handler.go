b79f4458b54e37d3b8b4ce0bb6efb9fee33654c1
sushil-cmd-r <kandharesushil10@gmail.com>
2024-12-22 00:52:51 +0530

    implement create and get by id routes

--- order/handler.go
+++ order/handler.go
@@ -1,10 +1,15 @@
 package order
 
 import (
+	"encoding/json"
+	"errors"
 	"log/slog"
 	"net/http"
+	"strconv"
 
+	"github.com/google/uuid"
 	"github.com/sushil-cmd-r/orders-api/store"
+	"github.com/sushil-cmd-r/orders-api/store/model"
 )
 
 type Handler struct {
@@ -14,6 +19,30 @@ type Handler struct {
 
 func (h *Handler) Create(w http.ResponseWriter, r *http.Request) {
 	h.Logger.Info("create order")
+	var body struct {
+		CustomerId uuid.UUID        `json:"customer_id"`
+		LineItems  []model.LineItem `json:"line_items"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	order := &model.Order{
+		CustomerId: body.CustomerId,
+		LineItems:  body.LineItems,
+	}
+
+	err := h.Store.Orders.Insert(r.Context(), order)
+	if err != nil {
+		h.Logger.Error("create order", "error", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	return
 }
 
 func (h *Handler) List(w http.ResponseWriter, r *http.Request) {
@@ -22,6 +51,34 @@ func (h *Handler) List(w http.ResponseWriter, r *http.Request) {
 
 func (h *Handler) GetById(w http.ResponseWriter, r *http.Request) {
 	h.Logger.Info("get order by id")
+	id := r.PathValue("id")
+
+	orderId, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		h.Logger.Error("invalid order id")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	order, err := h.Store.Orders.SelectById(r.Context(), orderId)
+	if errors.Is(store.ErrNotExist, err) {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	} else if err != nil {
+		h.Logger.Error("select order", "error", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	data, err := json.Marshal(order)
+	if err != nil {
+		h.Logger.Error("select order", "error", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	w.Write(data)
 }
 
 func (h *Handler) UpdateById(w http.ResponseWriter, r *http.Request) {
--- store/postgres.go
+++ store/postgres.go
@@ -3,9 +3,13 @@ package store
 import (
 	"context"
 	"database/sql"
+	"errors"
+	"fmt"
 
 	"github.com/sushil-cmd-r/orders-api/store/model"
 )
+
+var ErrNotExist = errors.New("resource not found")
 
 type orderStore struct {
 	conn *sql.DB
@@ -16,10 +20,79 @@ func (s *orderStore) Select(ctx context.Context) ([]model.Order, error) {
 }
 
 func (s *orderStore) SelectById(ctx context.Context, id int64) (*model.Order, error) {
-	return nil, nil
+	tx, err := s.conn.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, fmt.Errorf("select error: %w", err)
+	}
+
+	defer tx.Rollback()
+	order := model.Order{}
+	q1 := `SELECT id, customer_id, created_at, shipped_at, completed_at FROM orders WHERE id = $1;`
+
+	err = tx.QueryRowContext(ctx, q1, id).Scan(&order.Id, &order.CustomerId, &order.CreatedAt, &order.ShippedAt, &order.CompletedAt)
+
+	if errors.Is(sql.ErrNoRows, err) {
+		return nil, ErrNotExist
+	} else if err != nil {
+		return nil, fmt.Errorf("select error: %w", err)
+	}
+
+	q2 := `SELECT item_id, price, quantity FROM line_items WHERE order_id = $1;`
+	rows, err := tx.QueryContext(ctx, q2, id)
+	if err != nil {
+		return nil, fmt.Errorf("select error: %w", err)
+	}
+
+	var lineItems []model.LineItem
+	for rows.Next() {
+		var lineItem model.LineItem
+		if err := rows.Scan(&lineItem.ItemId, &lineItem.Price, &lineItem.Quantity); err != nil {
+			return nil, fmt.Errorf("select error: %w", err)
+		}
+		lineItems = append(lineItems, lineItem)
+	}
+
+	order.LineItems = lineItems
+	return &order, nil
 }
 
 func (s *orderStore) Insert(ctx context.Context, order *model.Order) error {
+	tx, err := s.conn.BeginTx(ctx, nil)
+	if err != nil {
+		return fmt.Errorf("insert error: %w", err)
+	}
+
+	defer tx.Rollback()
+	q1 := `INSERT INTO orders (customer_id) VALUES ($1) RETURNING id;`
+
+	var orderId int
+	err = tx.QueryRowContext(ctx, q1, order.CustomerId).Scan(&orderId)
+	if err != nil {
+		return fmt.Errorf("insert error: %w", err)
+	}
+
+	q2 := `INSERT INTO line_items (price, quantity, order_id) VALUES `
+	vals := make([]any, 0)
+	for idx, item := range order.LineItems {
+		i := idx*3 + 1
+		if idx == len(order.LineItems)-1 {
+			q2 += fmt.Sprintf("($%d, $%d, $%d);", i, i+1, i+2)
+		} else {
+			q2 += fmt.Sprintf("($%d, $%d, $%d),", i, i+1, i+2)
+		}
+
+		vals = append(vals, item.Price, item.Quantity, orderId)
+	}
+
+	_, err = tx.Exec(q2, vals...)
+	if err != nil {
+		return fmt.Errorf("failed insert: %w", err)
+	}
+
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("failed to commit: %w", err)
+	}
+
 	return nil
 }
 
